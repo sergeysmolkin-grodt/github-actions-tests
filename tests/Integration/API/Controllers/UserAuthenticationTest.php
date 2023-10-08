5d6fe42f45199614d6c17ee7d22dd1d4d4164e37
sergeysmolkinhtml <37virs85@gmail.com>
2023-10-08 14:40:00 +0200

    fresh dev branch + tests

--- tests/Integration/API/Controllers/UserAuthenticationTest.php
+++ tests/Integration/API/Controllers/UserAuthenticationTest.php
@@ -339,7 +339,7 @@ final class UserAuthenticationTest extends TestCase
     public function testUserLoginWithDeviceDataSuccessfully()
     {
         $user = User::factory()->create([
-            'email' => fake()->email(),
+            'email' => fake()->regexify('^[A-Za-z0-9]{6}@(gmail\.com|email\.ua)$'),
             'password' => Hash::make('12345678'),
             'is_active' => 1
         ]);
