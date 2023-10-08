ed618b1ba885f5d8d2b722126b7dc44f74daf31d
sergeysmolkinhtml <37virs85@gmail.com>
2023-10-08 11:24:33 +0200

    fresh dev branch + tests

--- tests/Integration/API/Controllers/AppointmentTest.php
+++ tests/Integration/API/Controllers/AppointmentTest.php
@@ -165,6 +165,7 @@ final class AppointmentTest extends TestCase
         $appointment = array_merge(
             TestCase::getAppointmentData($this->teacher,$this->user), [
             'date' => '2024-12-28',
+
         ]);
 
         $response = $this->postAppointmentAsUser($appointment);
