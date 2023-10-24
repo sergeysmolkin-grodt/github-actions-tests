edb7176f807b82cf9bf9939238ff26125982b3e0
Sergey-Smolkin-Leasoft <143398413+Sergey-Smolkin-Leasoft@users.noreply.github.com>
2023-10-24 09:54:01 +0300

    commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // i wont see this $schedule->command('inspire')->hourly();
+        // i will see this $schedule->command('inspire')->hourly();
     }
 
     /**
